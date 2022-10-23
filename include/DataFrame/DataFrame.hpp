7e167bec213ceb54c6adeed983019224d04451d6
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2022-10-23 15:48:00 +0100

    added lock free queue

--- include/DataFrame/DataFrame.hpp
+++ include/DataFrame/DataFrame.hpp
@@ -1,7 +1,19 @@
 #ifndef DataFrame_hpp
 #define DataFrame_hpp
 #include "../../include/DataFrame/DataFrame.hpp"
+#include "opencv2/opencv.hpp"
 
-struct DataFrame {};
+using namespace std;
+using namespace cv;
+
+struct DataFrame {
+public:
+  DataFrame(){};
+  void updateFrame(vector<Point2f> newFrame);
+  vector<Point2f> getFrame();
+
+private:
+  vector<Point2f> frame;
+};
 
 #endif
--- library/DataFrame/DataFrame.cpp
+++ library/DataFrame/DataFrame.cpp
@@ -1,3 +1,11 @@
-
 #include "../../include/DataFrame/DataFrame.hpp"
 #include "spdlog/spdlog.h"
+
+void DataFrame::updateFrame(vector<Point2f> newFrame){
+
+};
+vector<Point2f> DataFrame::getFrame() {
+  vector<Point2f> tmp;
+  tmp.push_back({0.0, 0.0});
+  return tmp;
+};
