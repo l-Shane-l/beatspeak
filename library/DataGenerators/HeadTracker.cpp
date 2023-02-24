621dbe90a82df27a37ee72257db92a3a0572ff5e
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2023-02-24 00:56:41 +0000

    more preprocessing of image

--- library/DataGenerators/HeadTracker.cpp
+++ library/DataGenerators/HeadTracker.cpp
@@ -6,7 +6,12 @@
 void HeadTracker::trackHeadInFrame(Mat &FrameRGB, Mat &FrameGray) {
   Mat blurredGray;
   GaussianBlur(FrameGray, blurredGray, Size(5, 5), 0); // add Gaussian blur with kernel size of 5x5
-
+  equalizeHist(FrameGray, FrameGray);
+  Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
+erode(FrameGray, FrameGray, element);
+dilate(FrameGray, FrameGray, element);
+  // Apply adaptive thresholding to the grayscale image
+  // adaptiveThreshold(FrameGray, FrameGray, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 5);
   trackerData->frameGray = blurredGray; // use the blurred image for processing
   trackerData->frameRGB = FrameRGB;
 
